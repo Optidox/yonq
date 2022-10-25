86bdbd92a6a3a5c5dbabdc56024da81b4d43af4b
Matthew Sims <optidox@gmail.com>
2022-10-25 01:28:14 -0400

    login!

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+
